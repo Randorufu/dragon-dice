9d4a9c20f2acfb88ac06cb9282b1c4beb42e0156
Randorufu <randolph.za@gmail.com>
2023-08-14 03:46:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ function toggle_desc(id) {
      e.style.display = "block";
      x.style.display = "block";
  }
+
+function toggle_dragon(id) {
+     var e = document.getElementById(id);
+     var x = document.getElementById(id).parentNode;
+     var z = document.getElementById(id).parentNode.parentNode;
+     var children = x.children;
+     if(e.style.display == 'inline-grid') {
+        e.style.display = 'none';
+        z.style.display = 'none';
+     } else {
+        for (var i=0; i<children.length; i++) children[i].style.display="none";
+        e.style.display = 'inline-grid';
+        z.style.display = 'block';
+     }
+ }
